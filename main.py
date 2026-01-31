cb74ff7b700d02e4e32a5e9031d719ee244733e0
dorihumad-web <dorihumad@gmail.com>
2026-01-31 12:56:41 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,4 +7,4 @@ bot=commands.Bot(command_prefix='!' , intents=discord.Intents.default())
 async def on_ready():
     print(f'起動したyoooo                                                                                          制作者Dorihu116by {bot.user}')
 
-bot .run('MTQ1NDQ0NDA2MDI4MTQwNTQ1Mw.Gm4XMB.qryPw_WSWpXdwdLOa8PkSbtD_OzG4yIQ0KBMdU')
+bot .run('TOKEN')
