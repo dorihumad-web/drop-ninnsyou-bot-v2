69ccabf40e0d91f71d59b2f39811d2ba406ae684
dorihumad-web <dorihumad@gmail.com>
2026-01-31 13:02:10 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ bot=commands.Bot(command_prefix='!' , intents=discord.Intents.default())
 
 @bot.event
 async def on_ready():
-    print(f'起動したyoooo                                                                                          制作者Dorihu116by {bot.user}')
+    print(f'run!                                                                                          制作者Dorihu116by {bot.user}')
 
 bot .run('TOKEN')
